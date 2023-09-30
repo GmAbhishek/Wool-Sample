35343fc5e2bcc42da6417edd1d1df8b54109878b
Gm_Abhishek <gmabhishek2004@gmail.com>
2023-09-30 22:35:24 +0530

    Added Screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
 import 'package:flutter/material.dart';
-import 'screens/dashboard.dart'; // Import the MyApp widget from my_app.dart
+import 'screens/forgot.dart'; // Import the MyApp widget from my_app.dart
 
 void main() => runApp(MyApp());
